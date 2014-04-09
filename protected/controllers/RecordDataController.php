57d289562b68bce6ba5a73d584ddf582b48c98fc
Maikol <maikol.guzman@healthwise.org>
2014-04-09 10:16:40 -0600

    Revert changes in Record Data

--- protected/controllers/RecordDataController.php
+++ protected/controllers/RecordDataController.php
@@ -1,180 +1,233 @@
 <?php
 
-class RecordDataController extends Controller
-{
-	/**
-	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-	 * using two-column layout. See 'protected/views/layouts/column2.php'.
-	 */
-	public $layout='//layouts/column2';
+class RecordDataController extends Controller {
 
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
-	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-			'postOnly + delete', // we only allow deletion via POST request
-		);
-	}
+    /**
+     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+     * using two-column layout. See 'protected/views/layouts/column2.php'.
+     */
+    public $layout = '//layouts/column2';
 
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'roles'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
-	}
+    /**
+     * @return array action filters
+     */
+    public function filters() {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+            'postOnly + delete', // we only allow deletion via POST request
+        );
+    }
 
-	/**
-	 * Displays a particular model.
-	 * @param integer $id the ID of the model to be displayed
-	 */
-	public function actionView($id)
-	{
-		$this->render('view',array(
-			'model'=>$this->loadModel($id),
-		));
-	}
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules() {
+        return array(
+            array('allow', // allow all users to perform 'index' and 'view' actions
+                'actions' => array('index', 'view'),
+                'users' => array('*'),
+            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'actions' => array('create', 'update', 'populateWOD'),
+                'users' => array('@'),
+            ),
+            array('allow', // allow admin user to perform 'admin' and 'delete' actions
+                'actions' => array('admin', 'delete'),
+                'roles' => array('admin'),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
 
-	/**
-	 * Creates a new model.
-	 * If creation is successful, the browser will be redirected to the 'view' page.
-	 */
-	public function actionCreate()
-	{
-		$model=new RecordData;
+    /**
+     * Displays a particular model.
+     * @param integer $id the ID of the model to be displayed
+     */
+    public function actionView($id) {
+        $this->render('view', array(
+            'model' => $this->loadModel($id),
+        ));
+    }
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+    /**
+     * Creates a new model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     */
+    public function actionCreate() {
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-		if (isset($_POST['RecordData'])) {
-			$model->attributes=$_POST['RecordData'];
-			if ($model->save()) {
-				$this->redirect(array('view','id'=>$model->id));
-			}
-		}
+        if (isset($_POST['RecordData'])) {
+            $total_workdetails = $_POST['total_workdetails'];
 
-		$this->render('create',array(
-			'model'=>$model,
-		));
-	}
+            for ($i = 0; $i < $total_workdetails; $i++) {
+                $work = 'WorkoutDetails' . $i;
 
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
-	public function actionUpdate($id)
-	{
-		$model=$this->loadModel($id);
+                $model = new RecordData;
+                $model->attributes = $_POST['RecordData'];
+                if (array_key_exists($work, $_POST)) {
+                    $work_array = $_POST[$work];
+                    $model->weight = (array_key_exists('weight', $work_array) ? $work_array['weight'] : 0);
+                    $model->height = (array_key_exists('height', $work_array) ? $work_array['height'] : 0);
+                    $model->assist = (array_key_exists('assist', $work_array) ? $work_array['assist'] : 0);
+                    $model->calories = (array_key_exists('calories', $work_array) ? $work_array['calories'] : 0);
+                    $model->reps = 0;
+                    $model->workout_detailid = $work_array['id'];
+                }
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+                $model->save();
 
-		if (isset($_POST['RecordData'])) {
-			$model->attributes=$_POST['RecordData'];
-			if ($model->save()) {
-				$this->redirect(array('view','id'=>$model->id));
-			}
-		}
+                var_dump($model);
+            }
 
-		$this->render('update',array(
-			'model'=>$model,
-		));
-	}
+            //$this->redirect(array('view','id'=>$model->id));
+        } else {
+            $model = new RecordData;
 
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'admin' page.
-	 * @param integer $id the ID of the model to be deleted
-	 */
-	public function actionDelete($id)
-	{
-		if (Yii::app()->request->isPostRequest) {
-			// we only allow deletion via POST request
-			$this->loadModel($id)->delete();
+            $this->render('create', array(
+                'model' => $model,
+            ));
+        }
+    }
 
-			// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-			if (!isset($_GET['ajax'])) {
-				$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-			}
-		} else {
-			throw new CHttpException(400,'Invalid request. Please do not repeat this request again.');
-		}
-	}
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     */
+    public function actionUpdate($id) {
+        $model = $this->loadModel($id);
 
-	/**
-	 * Lists all models.
-	 */
-	public function actionIndex()
-	{
-		$dataProvider=new CActiveDataProvider('RecordData');
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
-		));
-	}
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-	/**
-	 * Manages all models.
-	 */
-	public function actionAdmin()
-	{
-		$model=new RecordData('search');
-		$model->unsetAttributes();  // clear any default values
-		if (isset($_GET['RecordData'])) {
-			$model->attributes=$_GET['RecordData'];
-		}
+        if (isset($_POST['RecordData'])) {
+            $model->attributes = $_POST['RecordData'];
+            if ($model->save()) {
+                $this->redirect(array('view', 'id' => $model->id));
+            }
+        }
 
-		$this->render('admin',array(
-			'model'=>$model,
-		));
-	}
+        $this->render('update', array(
+            'model' => $model,
+        ));
+    }
 
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 * @param integer $id the ID of the model to be loaded
-	 * @return RecordData the loaded model
-	 * @throws CHttpException
-	 */
-	public function loadModel($id)
-	{
-		$model=RecordData::model()->findByPk($id);
-		if ($model===null) {
-			throw new CHttpException(404,'The requested page does not exist.');
-		}
-		return $model;
-	}
+    /**
+     * Deletes a particular model.
+     * If deletion is successful, the browser will be redirected to the 'admin' page.
+     * @param integer $id the ID of the model to be deleted
+     */
+    public function actionDelete($id) {
+        if (Yii::app()->request->isPostRequest) {
+            // we only allow deletion via POST request
+            $this->loadModel($id)->delete();
 
-	/**
-	 * Performs the AJAX validation.
-	 * @param RecordData $model the model to be validated
-	 */
-	protected function performAjaxValidation($model)
-	{
-		if (isset($_POST['ajax']) && $_POST['ajax']==='record-data-form') {
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-	}
+            // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+            if (!isset($_GET['ajax'])) {
+                $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+            }
+        } else {
+            throw new CHttpException(400, 'Invalid request. Please do not repeat this request again.');
+        }
+    }
+
+    /**
+     * Lists all models.
+     */
+    public function actionIndex() {
+        $criteria = new CDbCriteria();
+        $criteria->group = 'athleteid, date';
+        $dataProvider = new CActiveDataProvider('RecordData', array(
+            'criteria' => $criteria
+        ));
+        
+        
+        if (isset($_POST['RecordData'])) {
+            $total_workdetails = $_POST['total_workdetails'];
+
+            for ($i = 0; $i < $total_workdetails; $i++) {
+                $work = 'WorkoutDetails' . $i;
+
+                $model = new RecordData;
+                $model->attributes = $_POST['RecordData'];
+                if (array_key_exists($work, $_POST)) {
+                    $work_array = $_POST[$work];
+                    $model->weight = (array_key_exists('weight', $work_array) ? $work_array['weight'] : 0);
+                    $model->height = (array_key_exists('height', $work_array) ? $work_array['height'] : 0);
+                    $model->assist = (array_key_exists('assist', $work_array) ? $work_array['assist'] : 0);
+                    $model->calories = (array_key_exists('calories', $work_array) ? $work_array['calories'] : 0);
+                    $model->reps = 0;
+                    $model->workout_detailid = $work_array['id'];
+                }
+
+                $model->save();
+            }
+
+            //$this->redirect(array('view','id'=>$model->id));
+        }
+        
+        $model = new RecordData;
+        
+        $this->render('index', array(
+            'dataProvider' => $dataProvider,
+            'model' => $model,
+        ));
+    }
+
+    /**
+     * Manages all models.
+     */
+    public function actionAdmin() {
+        $model = new RecordData('search');
+        $model->unsetAttributes();  // clear any default values
+        if (isset($_GET['RecordData'])) {
+            $model->attributes = $_GET['RecordData'];
+        }
+
+        $this->render('admin', array(
+            'model' => $model,
+        ));
+    }
+
+    /**
+     * Returns the data model based on the primary key given in the GET variable.
+     * If the data model is not found, an HTTP exception will be raised.
+     * @param integer $id the ID of the model to be loaded
+     * @return RecordData the loaded model
+     * @throws CHttpException
+     */
+    public function loadModel($id) {
+        $model = RecordData::model()->findByPk($id);
+        if ($model === null) {
+            throw new CHttpException(404, 'The requested page does not exist.');
+        }
+        return $model;
+    }
+
+    /**
+     * Performs the AJAX validation.
+     * @param RecordData $model the model to be validated
+     */
+    protected function performAjaxValidation($model) {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'record-data-form') {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+    }
+
+    public function actionPopulateWOD($id) {
+        $data = array();
+        $data["myValue"] = "Content updated in AJAX";
+
+        $workout = Workout::model()->with('workoutType', 'workoutDetails.exercise')->findByPk($id);
+        $data['workout'] = $workout;
+
+        $this->renderPartial('_form2', $data, false, true);
+    }
+
 }
--- protected/views/recordData/_form.php
+++ protected/views/recordData/_form.php
@@ -18,35 +18,49 @@
     <p class="help-block">Fields with <span class="required">*</span> are required.</p>
 
     <?php echo $form->errorSummary($model); ?>
+    
+    
 
-            <?php echo $form->textFieldControlGroup($model,'weight',array('span'=>3,'maxlength'=>10)); ?>
-
-            <?php echo $form->textFieldControlGroup($model,'height',array('span'=>3,'maxlength'=>10)); ?>
-
-            <?php echo $form->textFieldControlGroup($model,'calories',array('span'=>3,'maxlength'=>10)); ?>
-
-            <?php echo $form->textFieldControlGroup($model,'assist',array('span'=>3,'maxlength'=>10)); ?>
-
-            <?php echo $form->textFieldControlGroup($model,'reps',array('span'=>3)); ?>
-
-            <?php echo $form->timeField($model,'time',array('span'=>3)); ?>
-              <?php  echo $form->label($model,'athleteid'); ?>
-            <?php echo  $form->dropDownList($model, 'athleteid', CHtml::listData(Athlete::model()->findAll(), 'id', 'first_name')); ?>
-
-            <?php //echo $form->textFieldControlGroup($model,'athleteid',array('span'=>3)); ?>
-              <?php  echo $form->label($model,'workout_detailid'); ?>
-            <?php echo  $form->dropDownList($model, 'workout_detailid', CHtml::listData(WorkoutDetail::model()->findAll(), 'id', 'workoutid')); ?>
-            <?php //echo $form->textFieldControlGroup($model,'workout_detailid',array('span'=>3)); ?>
-
-             <?php echo $form->dateFieldControlGroup($model,'date',array('span'=>3)); ?>
-
-        <div class="form-actions">
-        <?php echo TbHtml::submitButton($model->isNewRecord ? 'Create' : 'Save',array(
-		    'color'=>TbHtml::BUTTON_COLOR_PRIMARY,
-		    'size'=>TbHtml::BUTTON_SIZE_LARGE,
-		)); ?>
+    <table>
+        <tr>
+            <th><?php echo $form->labelEx($model,'Date'); ?></th>
+            <th>WOD</th>
+            <th><?php echo $form->labelEx($model,'athleteid'); ?></th>
+            <th><?php echo $form->labelEx($model,'time'); ?></th>
+        </tr>
+        <tr>
+            <td><?php echo $form->textField($model,'date'); ?></td>
+            <td><?php 
+                $list=CHtml::listData(Workout::model()->findAll(), 'id', 'name');
+                $list['empty'] = 'select';
+                echo CHtml::dropDownList('wod', 'empty', $list);
+                ?>
+            </td>
+            <td><?php echo  $form->dropDownList($model, 'athleteid', CHtml::listData(Athlete::model()->findAll(), 'id', 'first_name')); ?></td>
+            <td><?php echo $form->timeField($model,'time',array('span'=>3)); ?></td>
+        </tr>
+    </table>
+    <div id="exercises">
+        
     </div>
-
+    
+    
+    <input type="submit" class="btn btn-primary btn-small" value="create" >
+    
     <?php $this->endWidget(); ?>
-
+    
 </div><!-- form -->
+
+<?php
+Yii::app()->clientScript->registerScript('settings-script', <<<EOD
+        $('#wod').change(function() {
+            var value = $(this).val();
+            $.get('populateWOD?id=' + value, function(data, textStatus, jqXHR) {
+                    $('#exercises').empty();
+                    $('#exercises').append(data);
+                }
+            );
+        });
+EOD
+);
+?>
